e114858f7515d0b8a59d6cdfd1723f64f78f6365
Tomás Lago <tomas.lago@mundo-r.com>
2022-02-23 12:21:22 +0100

    carousel

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
+import { Carousel } from 'react-responsive-carousel';
 import './App.css';
 import { Accordion } from './components/accordion/Accordion';
+import './components/carousel/carousel.css';
 
 function App() {
   const accordeonData = [
-    { id: 1, title: 'Título 1', description: 'Descipción del item 1' },
-    { id: 2, title: 'Título 2', description: 'Descipción del item 2' },
-    { id: 3, title: 'Título 3', description: 'Descipción del item 3' },
-    { id: 4, title: 'Título 4', description: 'Descipción del item 4' },
+    {
+      id: 1,
+      title: 'Título 1',
+      description: 'Descipción del item 1',
+      photo: 'imgs/cerveza.jpg',
+    },
+    {
+      id: 2,
+      title: 'Título 2',
+      description: 'Descipción del item 2',
+      photo: 'imgs/senderismo2.jpg',
+    },
+    {
+      id: 3,
+      title: 'Título 3',
+      description: 'Descipción del item 3',
+      photo: 'imgs/snorkel1.jpg',
+    },
+    {
+      id: 4,
+      title: 'Título 4',
+      description: 'Descipción del item 4',
+      photo: 'imgs/NA.png',
+    },
   ];
 
   return (
     <div className="App">
-      <h1>Un acordeon simple</h1>
+      <h2>Un acordeon simple</h2>
       {accordeonData.map((item) => (
         <Accordion
           key={item.id}
@@ -19,6 +41,31 @@ function App() {
           content={item.description}
         />
       ))}
+      <br />
+      <hr />
+      <br />
+      <div>
+        <h2>Un carousel simple</h2>
+        <Carousel dynamicHeight={true} showIndicators={false}>
+          {accordeonData.map((s, index) => (
+            <div key={index} className="slider-container">
+              {s.photo ? (
+                <img src={`${s.photo}`} alt={s.title} className="exp-picture" />
+              ) : (
+                <img
+                  src={'imgs/NA.png'}
+                  alt={s.title}
+                  className="exp-picture"
+                />
+              )}
+              <span className="legend" id="laetiqueta">
+                <p id="title-carousel-card"> {s.title}</p>
+                <p id="price-carousel-card">{s.description}</p>
+              </span>
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </div>
   );
 }
